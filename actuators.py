d719f11df115cf6db54757d9d7759ddcefc250c9
Anvar Sultanov <anv.sult@gmail.com>
2025-11-29 10:37:31 -0500

    Switch the buzzer to use gpizero like everything else

--- actuators.py
+++ actuators.py
@@ -1,7 +1,7 @@
 import logging
 import math
-import RPi.GPIO as GPIO
-from gpiozero import LED, AngularServo
+from gpiozero import LED, AngularServo, TonalBuzzer, OutputDevice
+from gpiozero.tones import Tone
 from time import sleep
 from gpiozero.pins.pigpio import PiGPIOFactory
 
@@ -9,12 +9,9 @@ class Actuators:
     def __init__(self, led_bcm: int, buzzer_bcm: int, servo_bcm: int):
         self.led = LED(led_bcm)
         
-        # Setup buzzer with PWM for tonal control
+        # Use TonalBuzzer for PWM control (compatible with gpiozero)
+        self.buzzer = TonalBuzzer(buzzer_bcm)
         self.buzzer_pin = buzzer_bcm
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.buzzer_pin, GPIO.OUT)
-        self.buzzer_pwm = GPIO.PWM(self.buzzer_pin, 2000)  # Start with 2000 Hz
-        self.buzzer_pwm.start(0)  # 0% duty cycle (off)
 
         # Initialize Servo
         # Note: Jitter is common with standard GPIO. 
@@ -44,21 +41,18 @@ class Actuators:
 
     def buzz_once(self, duration=0.2):
         """Play a sine wave tone"""
-        for x in range(0, 361):
-            sinVal = math.sin(x * (math.pi / 180))
-            toneVal = 2000 + sinVal * 500
-            self.buzzer_pwm.ChangeFrequency(toneVal)
-            self.buzzer_pwm.ChangeDutyCycle(50)  # Set duty cycle during tone
-            sleep(0.001)
-        self.buzzer_pwm.ChangeDutyCycle(0)  # Turn off
+        # Simple rising tone effect
+        for freq in range(1500, 2500, 100):
+            self.buzzer.play(Tone(freq))
+            sleep(0.02)
+        self.buzzer.stop()
 
     def alarm_active(self, state: bool):
         """Continuous alarm sound"""
         if state:
-            self.buzzer_pwm.ChangeFrequency(2000)
-            self.buzzer_pwm.ChangeDutyCycle(50)  # Turn on
+            self.buzzer.play(Tone(2000))  # 2000Hz tone
         else:
-            self.buzzer_pwm.ChangeDutyCycle(0)  # Turn off
+            self.buzzer.stop()
 
     def led_on(self):
         self.led.on()
@@ -68,9 +62,8 @@ class Actuators:
 
     def shutdown(self):
         self.led.off()
-        self.buzzer_pwm.ChangeDutyCycle(0)
-        self.buzzer_pwm.stop()
-        GPIO.cleanup(self.buzzer_pin)
+        self.buzzer.stop()
+        self.buzzer.close()
         self.servo.detach()
         self.servo.close()
         self.led.close()
